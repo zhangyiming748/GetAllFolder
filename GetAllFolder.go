626a927e49a0b7cf938625221d7863fb9a6aef65
zen <zhangymb@adtec.com.cn>
2023-04-04 17:48:42 +0800

    fix

--- GetAllFolder.go
+++ GetAllFolder.go
@@ -64,10 +64,10 @@ func ListFolders(dirname string) []string {
 		// log.Info.Println(filename)                                  //打印文件地址
 		if fi.IsDir() { //判断是否是文件夹 如果是继续调用把自己的地址作为参数继续调用
 			if strings.Contains(filename, "/.") {
-				slog.Debug("跳过隐藏文件夹", slog.Any("文件名", fi.Name()))
+				mylog.Debug("跳过隐藏文件夹", slog.Any("文件名", fi.Name()))
 				continue
 			}
-			slog.Info("获取到文件夹", slog.String("文件名", filename))
+			mylog.Info("获取到文件夹", slog.String("文件名", filename))
 			all = append(all, filename)
 			folders = append(folders, filename)
 			ListFolders(filename) //递归调用
