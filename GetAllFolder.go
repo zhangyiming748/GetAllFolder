a40eb94f67c20b7e17b1f88d992835fe9b5ab745
zen <zhangyiming748@gmail.com>
2023-04-16 17:33:39 +0800

    通过环境变量LEVEL接收日志等级

--- GetAllFolder.go
+++ GetAllFolder.go
@@ -49,21 +49,17 @@ func SetLog(level string) {
 	if err != nil {
 		panic(err)
 	}
-	//defer logf.Close() //如果不关闭可能造成内存泄露
 	mylog = slog.New(opt.NewJSONHandler(io.MultiWriter(logf, os.Stdout)))
 
 }
-func ListFolders(dirname, level string) []string {
-	SetLog(level)
-	return List(dirname)
-}
+
 func List(dirname string) []string {
+	SetLog(os.Getenv("LEVEL"))
 	fileInfos, _ := os.ReadDir(dirname)
 	var folders []string
 	for _, fi := range fileInfos {
 		filename := strings.Join([]string{dirname, fi.Name()}, string(os.PathSeparator)) //拼写当前文件夹中所有的文件地址
-		// log.Info.Println(filename)                                  //打印文件地址
-		if fi.IsDir() { //判断是否是文件夹 如果是继续调用把自己的地址作为参数继续调用
+		if fi.IsDir() {                                                                  //判断是否是文件夹 如果是继续调用把自己的地址作为参数继续调用
 			if strings.Contains(filename, "/.") {
 				mylog.Debug("跳过隐藏文件夹", slog.Any("文件名", fi.Name()))
 				continue
